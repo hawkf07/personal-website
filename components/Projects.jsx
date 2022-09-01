f503c29128c84301bc08935a727b271d1aaea5de
fikri <fikrifadilah299@gmail.com>
2022-09-01 15:52:59 +0700

    removing ugly project section

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -9,13 +9,10 @@ export function ProjectList() {
       <section name="projectList" className="container grid gap-5 items-center justify-center ">
         <div name="card-1" className="grid prose grid-cols-1 place-center bg-gray-700 p-5 rounded">
           <h2> Personal Website </h2>   
-          <Image src="/Website1.png" layout="responsive" width="300" height={"200"} />
           <p> my personal website with minimalist design and simple. this website use React,NextJs and Tailwindcss</p>
         </div>
         <div name="card-2" className="grid prose grid-cols-1 place-center bg-gray-700 p-5 rounded">
           <h2>NASA Media Library Searcher</h2>   
-
-          <Image src="/web2.png" layout="intrinsic" width="300" height={"300"} />
           <p>this consume NASA OPEN API,you can search a beautiful image and videos here.this project used react and tailwindcss to make things a lot faster and easiear</p>
         </div>
         <div name="card-2" className="grid my-5 prose grid-cols-1 place-center bg-gray-700 p-5 rounded">
--- scripts.sh
+++
@@ -1,2 +0,0 @@
-feh --bg-scale ~/Downloads/matt-palmer-M7EnqaUt6Yg-unsplash.jpg&
-sh ~/.config/chadwm/scripts/bar.sh
